804905c4e8759e533817de5d69f1309ee0b6d6d9
john <john@john-VirtualBox.(none)>
2023-09-17 13:32:27 -0400

    Adding a few coroutine tests.

---
+++ src/test/java/pile/core/CoroutineTest.java
@@ -0,0 +1,78 @@
+package pile.core;
+
+import static org.junit.Assert.*;
+import static pile.nativebase.NativeCore.*;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.Timeout;
+
+import pile.core.Coroutine.CoroutineSync;
+import pile.nativebase.NativeCore;
+
+public class CoroutineTest {
+
+    @Rule
+    public Timeout maxTime = new Timeout(5000);
+
+    private CoroutineSync sync;
+    private AtomicInteger calledCount;
+
+    @Test
+    public void test() throws InterruptedException {
+        var c = newCoroutine();
+        sleep(1000);
+        assertEquals(0, calledCount.get());
+        assertEquals(1, resume(c));
+        assertEquals(null, resume(c));
+    }
+
+    @Test
+    public void testRaceResume() throws InterruptedException {
+        // Testing to make sure race condition which resume beats a call to the first
+        // coroutine sleep (within run).
+        var c = newStoppedCoroutine();
+        Thread.startVirtualThread(() -> {
+            try {
+                sleep(1000);
+                c.run();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        assertEquals(1, resume(c));
+        assertEquals(null, resume(c));
+    }
+
+    public Coroutine newStoppedCoroutine() {
+        this.sync = new CoroutineSync();
+        this.calledCount = new AtomicInteger();
+
+        Coroutine c = new Coroutine(sync, new PCall() {
+            @Override
+            public Object invoke(Object... args) throws Throwable {
+                NativeCore.yield(calledCount.incrementAndGet());
+                return null;
+            }
+        });
+        return c;
+    }
+
+    public Coroutine newCoroutine() {
+        this.sync = new CoroutineSync();
+        this.calledCount = new AtomicInteger();
+
+        Coroutine c = new Coroutine(sync, new PCall() {
+            @Override
+            public Object invoke(Object... args) throws Throwable {
+                NativeCore.yield(calledCount.incrementAndGet());
+                return null;
+            }
+        });
+        c.run();
+        return c;
+    }
+
+}
